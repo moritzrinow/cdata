6170aee15f8114d79ea9ec9f03bf700f8dbf4ddf
Moritz Rinow <34030059+moritzrinow@users.noreply.github.com>
2019-06-03 11:10:27 +0200

    Tab fix

--- src/map.h
+++ src/map.h
@@ -25,7 +25,7 @@ typedef struct map_s {
   alloc_t            alloc;
   array_t            entries;
   map_func_t         func;
-  uint32_t					 num_elem;
+  uint32_t           num_elem;
 } map_t;
 
 typedef struct map_iterator_s {
@@ -60,7 +60,7 @@ bool map_rehash(map_t *map);
 typedef void (*map_foreach_key_func_t)(void *);
 typedef void (*map_foreach_key_val_func_t)(void *, void *);
 void map_foreach_key(map_t *map,
-		                 map_foreach_key_func_t action);
+                     map_foreach_key_func_t action);
 
 uint32_t map_key_hash_int8(void *);
 uint32_t map_key_hash_int16(void *);
